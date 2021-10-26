5d29ae2736d0301368f463ff06cfaca867992296
Chris <amrielsusanto@gmail.com>
2021-10-26 23:23:39 +0700

    minor change

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,13 +2,16 @@ import React from "react"
 import Navbar from "../components/Navbar.js"
 import '../styles/global.css'
 
+
 export default function Home( {data} ) {
   return (
     <div className="content">
       <Navbar/>
       <div className="about">
       <h3>Resume</h3>
-      <iframe width="600" height="600" src="resume.pdf"/>
+      <object data="resume.pdf" type="application/pdf" width="700px" height="700px">
+  </object>
+  <iframe width="600" height="600" src="resume.pdf"/>
       </div>
     </div>
   )
