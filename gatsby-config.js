51cc09dcfe4c4931fe29beb076182b1293f5bebb
Chris <amrielsusanto@gmail.com>
2021-10-26 14:06:44 +0700

    Lacking Resume
    Lack Styling
    
    Added Resume and Styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,11 +14,20 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/pages/images/`,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
       },
     },
   ],
---
+++ src/projects/water-reminder.md
@@ -0,0 +1,9 @@
+---
+title: Water Reminder
+stack: HTML, CSS, Javascript
+slug: water-reminder
+github: https://github.com/chrisamrl/waterReminder
+
+---
+
+A simple chrome extension that reminds you to drink water every 30 minutes. When the reminder is turned on, it will send you a notification to remind you to drink water. 
