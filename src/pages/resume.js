c9289d2b0e7aba16ae202f14a2cc63c444e30f10
Chris <amrielsusanto@gmail.com>
2021-10-26 23:42:45 +0700

    minor

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,12 +7,12 @@ export default function Home( {data} ) {
   return (
     <div className="content">
       <Navbar/>
-      <div className="about">
-      <h3>Resume</h3>
-      <object data="resume.pdf" type="application/pdf" width="700px" height="700px">
-  </object>
-  <iframe width="600" height="600" src="resume.pdf"/>
-      </div>
+        <div className="about">
+          <h3>Resume</h3>
+          <object data="resume.pdf" type="application/pdf" width="700px" height="700px">
+          <p>The browser does not support PDFs <a href="resume.pdf">Please download the PDF to view it: Download PDF</a></p>
+          </object>
+        </div>
     </div>
   )
 }
