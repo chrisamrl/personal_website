eccb9b4fb6b0c259d1ff16ff5cc2d0a1643e85a2
Chris <amrielsusanto@gmail.com>
2021-10-26 18:34:24 +0700

    Added Latex Link in Resume, Fixed mistyped Link in MD file

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,14 +2,17 @@ import React from "react"
 import Navbar from "../components/Navbar.js"
 import '../styles/global.css'
 
+
 export default function Home( {data} ) {
   return (
     <div className="content">
       <Navbar/>
-      <div className="about">
-      <h3>Resume</h3>
-      <iframe width="600" height="600" src="resume.pdf"/>
-      </div>
+        <div className="about">
+          <h3>Resume</h3>
+          <object data="resume.pdf" type="application/pdf" width="700px" height="700px">
+          <p>The browser does not support PDFs <a href="resume.pdf">Please download the PDF to view it: Download PDF</a></p>
+          </object>
+        </div>
     </div>
   )
 }
