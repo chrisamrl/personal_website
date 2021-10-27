d568612d39d8cea64f8b42c60e34c7dd68a40f72
Chris <amrielsusanto@gmail.com>
2021-10-27 11:13:50 +0700

    PDF is now queried from GraphQL, Previously from static folder

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,18 +1,28 @@
 import React from "react"
 import Navbar from "../components/Navbar.js"
 import '../styles/global.css'
+import { graphql } from "gatsby"
 
 
 export default function Home( {data} ) {
+  console.log(data)
   return (
     <div className="content">
       <Navbar/>
         <div className="about">
           <h3>Resume</h3>
-          <object data="resume.pdf" type="application/pdf" width="700px" height="700px">
-          <p>The browser does not support PDFs <a href="resume.pdf">Please download the PDF to view it: Download PDF</a></p>
+          <object data={data.file.publicURL} type="application/pdf" width="700px" height="800px">
+          <p>Your browser does not support PDFs. Please download the PDF to view it:<a href="resume.pdf"> Download PDF</a></p>
           </object>
         </div>
     </div>
   )
 }
+
+export const query = graphql`
+query ResumeInfo {
+  file(relativePath: {eq: "resume.pdf"}) {
+    publicURL
+  }
+}
+`
